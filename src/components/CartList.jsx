3718a098c0e57a2fab0d6cf6df982b09543a1448
unknown <jthdt4827@hotmail.com>
2026-07-21 09:41:52 +0100

    db json

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -3,9 +3,29 @@ import { CartContext } from "../cart-context";
 import ItemCard from "./ItemCard";
 
 function CartList() {
-    const { itemsInCart, calculateTotal } = useContext(CartContext)
+    const { itemsInCart, setItemsInCart, calculateTotal } = useContext(CartContext)
 
-    const order_items = []
+    const handleCheckout = async () => {
+        const order = {
+            items: itemsInCart,
+            total: calculateTotal
+        }
+
+        const response = await fetch('https://localhost:8080/orders', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        }
+        )
+
+        if (!response.ok) {
+            throw new Error('Checkout failed')
+        }
+
+        setItemsInCart([]);
+    }
     // loop around the itemsId collection from context
     // use getItemById to get items and push them onto the order_items
     // create order object that contains the order_items and the order_total
@@ -27,6 +47,10 @@ function CartList() {
             </div>
 
             <h2>Total Price: £{calculateTotal().toFixed(2)}</h2>
+
+            <button onClick={handleCheckout}>
+                Checkout
+            </button>
 
         </>
     )
